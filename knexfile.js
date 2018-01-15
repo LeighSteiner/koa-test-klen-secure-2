c1862909b5eb8ce607e50b266710cfa930a788b8
Leigh Steiner <steinerleigh@gmail.com>
2018-01-15 17:28:03 -0500

    starting routes

---
+++ knexfile.js
@@ -0,0 +1,30 @@
+
+const path = require('path');
+
+const BASE_PATH = path.join(__dirname, 'src', 'server', 'db');
+
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: 'postgres://username:postgres:fred10@localhost:5432/koa_api_test',
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    }
+  },
+  development: {
+    client: 'pg',
+     connection: 'postgres://postgres:fred10@localhost:5432/koa_api', 
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    }
+  }
+};
+
+
+ 
--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,22 @@
-console.log('It works!');
+const Koa = require('koa');
+const indexRoutes = require('./routes/index');
+const movieRoutes = require('./routes/movies');
+
+const app = new Koa();
+const PORT = 1337; 
+
+app.use(indexRoutes.routes());
+app.use(movieRoutes.routes());
+
+// app.use(async (ctx) => {
+//   ctx.body = {
+//   	status: 'success', 
+//   	message: 'hello, world!',
+//   };
+// });
+
+const server = app.listen(PORT, () => {
+	console.log(`Koa server listening on port: ${PORT}`)
+});
+
+module.exports = server;
